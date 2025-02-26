f625b78ba1e84130dfefa669f45aec51fcb90775
Harshith Ashvi <harshith.ashvi@gmail.com>
2025-02-26 08:03:04 +0530

    add required package, create express app and listen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("VideoTube");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 8001;
+app.listen(PORT, () => {
+  console.log(`Server is running on Port: ${PORT}`);
+});
