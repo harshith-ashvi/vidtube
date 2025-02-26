527c0ea5273e56ee10b97aa6fb64e7d5b2f12de9
Harshith Ashvi <harshith.ashvi@gmail.com>
2025-02-26 18:36:03 +0530

    add asyncHandler, ApiResponse, ApiError and add healthcheck controller, route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 
+import healthcheckRouter from "./routes/healthcheck.js";
+
 const app = express();
 
 app.use(
@@ -14,5 +16,7 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./.env",
 });
 
 const PORT = process.env.PORT || 8001;
-app.listen(PORT, () => {
-  console.log(`Server is running on Port: ${PORT}`);
-});
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on Port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error ", err);
+  });
